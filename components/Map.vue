fda22cd196d9443b8a43844a6e7d289d79124ba4
tfjackc <jack.colpitt@crookcountyor.gov>
2023-11-25 21:37:21 -0800

    first commit

---
+++ components/Map.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="height:100vh; width:100vw">
+    <LMap
+        ref="map"
+        :zoom="zoom"
+        :center="[46.982639, -108.519417]"
+    >
+      <LTileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+          layer-type="base"
+          name="OpenStreetMap"
+      />
+    </LMap>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const zoom = ref(4)
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AppBar from "~/components/AppBar.vue";
+</script>
+
+<template>
+  <AppBar />
+<slot />
+</template>
+
+<style scoped>
+
+</style>
